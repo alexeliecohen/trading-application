241c939c7e25389a773e5505b559be043a59d59f
alexeliecohen <work.alex.c@gmail.com>
2022-07-11 16:23:01 -0400

    Working with backpressure error

--- src/test/java/services/BackPressureTest.java
+++ src/test/java/services/BackPressureTest.java
@@ -1,5 +1,6 @@
 package services;
 
+import org.intellij.lang.annotations.JdkConstants;
 import org.junit.jupiter.api.Test;
 import org.reactivestreams.Subscription;
 import reactor.core.publisher.BaseSubscriber;
@@ -50,11 +51,12 @@ public class BackPressureTest {
     });
   }
 
+  @Test
   public void testBackPressureOnDrop() {
     var numbers = Flux.range(1, 100).log();
     numbers
         .onBackpressureDrop(integer -> {
-          System.out.printf("Dropped Values = %d", integer);
+          System.out.printf("Dropped Values = %d\n", integer);
           ;
         })
         .subscribe(new BaseSubscriber<Integer>() {
@@ -81,6 +83,92 @@ public class BackPressureTest {
           @Override
           protected void hookOnError(Throwable throwable) {
             super.hookOnError(throwable);
+          }
+
+          @Override
+          protected void hookOnCancel() {
+            super.hookOnCancel();
+          }
+
+          @Override
+          protected void hookFinally(SignalType type) {
+            super.hookFinally(type);
+          }
+        });
+  }
+
+  @Test
+  public void testBackPressureOnBuffer() {
+    var numbers = Flux.range(1, 100).log();
+    numbers
+        .onBackpressureBuffer(10, (integer)-> {
+          System.out.printf("Buffered = %d\n",integer);
+        })
+        .subscribe(new BaseSubscriber<Integer>() {
+          @Override
+          protected void hookOnSubscribe(Subscription subscription) {
+            request(3);
+          }
+
+          @Override
+          protected void hookOnNext(Integer value) {
+            System.out.printf("value = %d\n", value);
+            if (value == 3) {
+              hookOnCancel();
+            }
+            super.hookOnNext(value);
+          }
+
+          @Override
+          protected void hookOnComplete() {
+            System.out.printf("Completed\n");
+            super.hookOnComplete();
+          }
+
+          @Override
+          protected void hookOnError(Throwable throwable) {
+            super.hookOnError(throwable);
+          }
+
+          @Override
+          protected void hookOnCancel() {
+            super.hookOnCancel();
+          }
+
+          @Override
+          protected void hookFinally(SignalType type) {
+            super.hookFinally(type);
+          }
+        });
+  }
+  @Test
+  public void testBackPressureError() {
+    var numbers = Flux.range(1, 100).log();
+    numbers
+        .onBackpressureError()
+        .subscribe(new BaseSubscriber<Integer>() {
+          @Override
+          protected void hookOnSubscribe(Subscription subscription) {
+            request(3);
+          }
+
+          @Override
+          protected void hookOnNext(Integer value) {
+            System.out.printf("value = %d\n", value);
+            if (value == 3) {
+              hookOnCancel();
+            }
+          }
+
+          @Override
+          protected void hookOnComplete() {
+            System.out.printf("Completed\n");
+            super.hookOnComplete();
+          }
+
+          @Override
+          protected void hookOnError(Throwable throwable) {
+            System.out.println("throwable = " + throwable);
           }
 
           @Override

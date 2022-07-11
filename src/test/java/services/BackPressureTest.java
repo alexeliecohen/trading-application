c0dcdd533cd4d7b7c0e8d71f7adfd07d00d79084
alex-cohen-adaptive <alex.cohen@weareadaptive.com>
2022-07-11 13:00:26 -0400

    test commit

--- src/test/java/services/BackPressureTest.java
+++ src/test/java/services/BackPressureTest.java
@@ -61,6 +61,7 @@ public class BackPressureTest {
           @Override
           protected void hookOnSubscribe(Subscription subscription) {
             request(3);
+            System.out.println("hello world");
           }
 
           @Override
